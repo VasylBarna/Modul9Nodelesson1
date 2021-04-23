43a31e3801ee29b95d9ef252339bb8691228d049
VasylBarna <vasylbarna@gmail.com>
2021-04-23 15:32:35 +0300

    Modul9Node-lesson1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// const { writeFile, readFile } = require('./tasks/task1');
+// // const task1 = require('./tasks/task1');
+
+
+// writeFile("this is my text");
+
+// console.log(readFile());
+
+const app = require("./tasks/task2");
